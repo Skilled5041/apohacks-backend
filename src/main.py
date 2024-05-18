416d0bb9ee2c2f3689160168e354545af4765b37
Emilio Mendoza <ironislife39@gmail.com>
2024-05-17 22:27:18 -0400

    added live stt lib

--- src/main.py
+++ src/main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI, File, UploadFile
-from livestt import transcribe
+import livestt
 
 app = FastAPI()
+
+curr_state = False
+
 
 @app.get('/')
 async def home():
     return {"Hello": "World"}
 
+@app.get("/state/")
+async def state():
+    return {"state": curr_state}
+
+@app.get("/toggle/")
+async def toggle():
+    global curr_state
+    curr_state = not curr_state
+    return {}
+
 @app.get("/upload/")
-async def uploadtext: str):
+async def upload(text: str):
     return {"msg": "Success"}
