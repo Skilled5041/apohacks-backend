98d6df36d1661e9480619daaf2c96a2d2c8fe197
Emilio Mendoza <ironislife39@gmail.com>
2024-05-17 22:16:49 -0400

    Basic state

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from livestt import transcribe
 
 app = FastAPI()
 
 @app.get('/')
 async def home():
     return {"Hello": "World"}
+
+@app.get("/upload/")
+async def uploadtext: str):
+    return {"msg": "Success"}
