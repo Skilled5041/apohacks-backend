a93a5e107c2b3541542c10c066c66340357a16a8
James Cao <jamescao800@gmail.com>
2024-05-18 18:00:14 -0400

    added stuff

--- src/main.py
+++ src/main.py
@@ -3,6 +3,90 @@ from livestt.livestt import Recorder, transcribe
 import threading
 
 
+from elevenlabs import play, save
+from elevenlabs.client import ElevenLabs
+from pydub import AudioSegment
+import numpy as np
+import scipy.signal
+import soundfile as sf
+from pydub.utils import which
+import requests
+import numpy as np
+import sounddevice as sd 
+
+
+
+apikey = "8a887d470693e5102aa7baf862b547d5"
+
+client = ElevenLabs(
+  api_key=apikey
+)
+
+url = "https://api.elevenlabs.io/v1/text-to-speech/{voice_id}"
+
+
+client = ElevenLabs(
+  api_key=apikey, # Defaults to ELEVEN_API_KEY
+)
+
+def zombienoise(text):
+  audio = client.generate(
+    text=text,
+    voice="Jessie",
+    model="eleven_multilingual_v2"
+  )
+  name = "23"
+  audio = client.generate(
+    text=text,
+    voice="Jessie",
+    model="eleven_multilingual_v2"
+  )
+  filename = f"{name}.mp3"
+  save(audio, filename)
+  audio = AudioSegment.from_mp3(f"{name}.mp3")
+  slowed_audio = slowDown(audio, 5)
+  # slowed_audio.export(f"{filename}new.wav", format="wav")
+  # slowedaudio, factor = sf.read(f"{filename}new.wav")
+  # distorted_audio_data = add_dis(slowedaudio)
+  # sf.write(f"{filename}zombie.wav", distorted_audio_data, factor)
+  # zombieaudio = AudioSegment.from_file(f"{filename}zombie.wav")
+  # audioData  = np.array(zombieaudio.get_array_of_samples())
+  audioData  = np.array(slowed_audio.get_array_of_samples())
+  sd.play(audioData, samplerate = audio.frame_rate)
+  sd.wait()
+
+
+def pplnoise(text):
+  audio = client.generate(
+    text=text,
+    voice="Jessie",
+    model="eleven_multilingual_v2"
+  )
+  name = input("Enter the name of the file: ")
+  filename = f"{name}.mp3"
+  save(audio, filename)
+  audio = AudioSegment.from_mp3(f"{name}.mp3")
+  audioData  = np.array(audio.get_array_of_samples())
+  sd.play(audioData, samplerate = audio.frame_rate)
+  sd.wait()
+  
+def changep(audio, semitones):
+  newRate = int(audio.frame_rate * 2**(semitones/12)) 
+  newaudio = audio._spawn(audio.raw_data, overrides={'frame_rate': newRate})
+  return newaudio.set_frame_rate(audio.frame_rate)
+
+def slowDown(audio, factor):
+  newaudio = audio._spawn(audio.raw_data, overrides={'frame_rate': int(audio.frame_rate*factor)})
+  return newaudio.set_frame_rate(audio.frame_rate *factor)
+
+def add_dis(audio, gain=1.5, threshold=0.5):
+  newaudio = audio * gain
+  newaudio[newaudio > threshold] = threshold
+  newaudio[newaudio < -threshold] = -threshold
+  return newaudio
+
+
+
 app = FastAPI()
 
 curr_state = False
@@ -11,43 +95,13 @@ filename = "test.wav"
 recorder = Recorder(filename)
 
 
-def ModdedCeaserCipher(text: str) -> str:
-    c_map = {
-        "A": "grr",
-        "B": "argh",
-        "C": "ugh",
-        "D": "rawr",
-        "E": "brr",
-        "F": "gah",
-        "G": "urr",
-        "H": "blur",
-        "I": "hmm",
-        "J": "zzz",
-        "K": "rarr",
-        "L": "blargh",
-        "M": "snar",
-        "N": "arg",
-        "O": "mur",
-        "P": "grar",
-        "Q": "urgh",
-        "R": "blurr",
-        "S": "snarl",
-        "T": "garr",
-        "U": "hur",
-        "V": "braar",
-        "W": "snur",
-        "X": "grargh",
-        "Y": "grur",
-        "Z": "arrgh",
-        " ": "hrr"
-    }
+def ModdedCeaserCipher(text: str, shift: int) -> str:
+    allowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     o = ""
     for c in text:
-        if c.upper() in c_map:
-            o += c_map[c.upper()]
+        if c not in allowed:
+            o += " "
             continue
-        o += " "
-    return o
 
 
 
@@ -62,8 +116,8 @@ def listener():
         elif not curr_state and started:
             recorder.end()
             started = False
-            text = list(transcribe(filename))[0]
-            print(ModdedCeaserCipher(text.text))
+            for t in transcribe(filename):
+                print(t.text)
 
 
 thread = threading.Thread(target=listener)
