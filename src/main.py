8dc096910d4c29ee164bce1f45d7c762ff288996
Emilio Mendoza <ironislife39@gmail.com>
2024-05-17 23:03:53 -0400

    Transcription done

--- src/main.py
+++ src/main.py
@@ -1,9 +1,43 @@
 from fastapi import FastAPI, File, UploadFile
-import livestt
+from livestt.livestt import Recorder, transcribe
+import threading
+
 
 app = FastAPI()
 
 curr_state = False
+
+filename = "test.wav"
+recorder = Recorder(filename)
+
+
+def ModdedCeaserCipher(text: str, shift: int) -> str:
+    allowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    o = ""
+    for c in text:
+        if c not in allowed:
+            o += " "
+            continue
+
+
+
+
+def listener():
+    started = False
+    while True:
+        #print(curr_state, started)
+        if curr_state and not started:
+            recorder.start()
+            started = True
+        elif not curr_state and started:
+            recorder.end()
+            started = False
+            for t in transcribe(filename):
+                print(t.text)
+
+
+thread = threading.Thread(target=listener)
+thread.start()
 
 
 @app.get('/')
